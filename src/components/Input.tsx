6770b7a193b611eac51f9dbdb9a997199f4b90d9
keewonf <lukasfmm@gmail.com>
2026-04-28 07:36:16 -0300

    feat(auth): add Input component and update SignIn form

---
+++ src/components/Input.tsx
@@ -0,0 +1,20 @@
+type Props = React.ComponentProps<"input"> & {
+  legend?: string;
+};
+
+export function Input({ legend, ...rest }: Props) {
+  return (
+    <fieldset className="flex flex-1 max-h-20 text-gray-200 focus-within:text-green-100">
+      {legend && (
+        <legend className="uppercase  text-xxs mb-2 text-inherit">
+          {legend}
+        </legend>
+      )}
+
+      <input
+        className="border border-gray-300 rounded-lg px-4 py-[15px] w-full h-12 text-sm text-gray-100 bg-transparent outline-none focus:border-2 focus:border-green-100 placeholder-gray-300"
+        {...rest}
+      />
+    </fieldset>
+  );
+}
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,3 +1,10 @@
-export function SignIn(){
-  return <h1>SignIn</h1>
+import { Input } from "../components/Input";
+
+export function SignIn() {
+  return (
+    <form className="w-full flex flex-col gap-4">
+      <Input required type="email" legend="E-MAIL" placeholder="seu@email.com"/>
+      <Input required legend="SENHA" type="password" placeholder="123456"/>
+    </form>
+  );
 }
