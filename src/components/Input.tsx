0c8ddb7cffd73d96ef1d58d3d0acb270506add43
keewonf <lukasfmm@gmail.com>
2026-04-28 08:20:05 -0300

    fix(input): accept type prop and adjust vertical padding

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,7 +2,7 @@ type Props = React.ComponentProps<"input"> & {
   legend?: string;
 };
 
-export function Input({ legend, ...rest }: Props) {
+export function Input({ legend, type = "text", ...rest }: Props) {
   return (
     <fieldset className="flex flex-1 max-h-20 text-gray-200 focus-within:text-green-100">
       {legend && (
@@ -12,7 +12,8 @@ export function Input({ legend, ...rest }: Props) {
       )}
 
       <input
-        className="border border-gray-300 rounded-lg px-4 py-[15px] w-full h-12 text-sm text-gray-100 bg-transparent outline-none focus:border-2 focus:border-green-100 placeholder-gray-300"
+        type={type}
+        className="border border-gray-300 rounded-lg px-4 py-3.75 w-full h-12 text-sm text-gray-100 bg-transparent outline-none focus:border-2 focus:border-green-100 placeholder-gray-300"
         {...rest}
       />
     </fieldset>
