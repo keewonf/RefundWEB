2f068cabb2b19405309350047fe703c4717ba39b
keewonf <lukasfmm@gmail.com>
2026-05-02 00:31:45 -0300

    fix(app-layout): adjust main container max-width for consistency

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -5,7 +5,7 @@ import { Header } from "./Header";
 export function AppLayout() {
   return (
     <div className="w-screen min-h-screen bg-gray-400  flex flex-col items-center text-gray-100">
-      <main className="p-3 w-full md:w-auto">
+      <main className="p-3 w-full max-w-296.25 ">
         <Header />
         <Outlet />
       </main>
