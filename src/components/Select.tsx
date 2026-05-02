4a0d3584a194cda661b0081fac8c6ce371a5f3d3
keewonf <lukasfmm@gmail.com>
2026-05-02 07:07:18 -0300

    style(select/refund): adjust Select styling and refund layout/values

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -12,7 +12,7 @@ export function Select({ legend, children, ...rest }: Props) {
       )}
 
       <select
-        className="w-full h-12 rounded-lg border border-gray-300 px-4 text-sm text-gray-100 bg-transparent outline-none focus:border-2 focus:border-green-100 placeholder-gray-300"
+        className={`w-full h-12 rounded-lg border border-gray-300 px-4 text-sm  bg-transparent outline-none focus:border-2 focus:border-green-100 placeholder-gray-300 ${rest.value === "" ? "text-gray-200" : "text-gray-100"}`}
         {...rest}
       >
         <option value="" disabled hidden>
