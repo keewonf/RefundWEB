7df75ffac605d8b3157ea621d00c7b31d916d662
keewonf <lukasfmm@gmail.com>
2026-05-05 10:14:10 -0300

    feat(routes): select route tree by user role

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,36 @@
 import { BrowserRouter } from "react-router";
 
-import { Loading} from "../components/Loading"
+import { Loading } from "../components/Loading";
 
 import { AuthRoutes } from "./AuthRoutes";
 import { EmployeeRoutes } from "./EmployeeRoutes";
 import { ManagerRoutes } from "./ManagerRoutes";
 
-const isLoading = false
+const isLoading = false;
+const session = {
+  user: {
+    role: "manager",
+  },
+};
 
 export function Routes() {
-  if(isLoading){
-    return <Loading/>
+  function Route() {
+    switch (session.user.role) {
+      case "manager":
+        return <ManagerRoutes/>
+      case "employee":
+        return <EmployeeRoutes/>
+      default:
+        return <AuthRoutes/>
+    }
   }
 
-
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
     <BrowserRouter>
-      <AuthRoutes />
+      <Route/>
     </BrowserRouter>
   );
 }
