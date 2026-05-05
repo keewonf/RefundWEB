2a3d3c089ff9e83e14b464c4dba0da998a50a2c1
keewonf <lukasfmm@gmail.com>
2026-05-05 10:00:51 -0300

    feat(routes): add loading screen and render auth routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,22 @@
 import { BrowserRouter } from "react-router";
 
+import { Loading} from "../components/Loading"
+
 import { AuthRoutes } from "./AuthRoutes";
 import { EmployeeRoutes } from "./EmployeeRoutes";
 import { ManagerRoutes } from "./ManagerRoutes";
 
+const isLoading = false
+
 export function Routes() {
+  if(isLoading){
+    return <Loading/>
+  }
+
+
   return (
     <BrowserRouter>
-      <ManagerRoutes />
+      <AuthRoutes />
     </BrowserRouter>
   );
 }
