43727b2053773702a56744249c25db0c46f797bc
keewonf <lukasfmm@gmail.com>
2026-05-06 10:42:42 -0300

    feat(input): support validation error prop and remove fixed fieldset max height

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,10 +1,11 @@
 type Props = React.ComponentProps<"input"> & {
   legend?: string;
+  error?: string;
 };
 
-export function Input({ legend, type = "text", ...rest }: Props) {
+export function Input({ legend, error, type = "text", ...rest }: Props) {
   return (
-    <fieldset className="flex flex-1 max-h-20 text-gray-200 focus-within:text-green-100">
+    <fieldset className="flex flex-1 text-gray-200 focus-within:text-green-100 flex-col">
       {legend && (
         <legend className="uppercase  text-xxs mb-2 text-inherit">
           {legend}
@@ -13,9 +14,12 @@ export function Input({ legend, type = "text", ...rest }: Props) {
 
       <input
         type={type}
-        className="border border-gray-300 rounded-lg px-4 py-3.75 w-full h-12 text-sm text-gray-100 bg-transparent outline-none focus:border-2 focus:border-green-100 placeholder-gray-300"
+        className={`border border-gray-300 rounded-lg px-4 py-3.75 w-full h-12 text-sm text-gray-100 bg-transparent outline-none focus:border-2 focus:border-green-100 placeholder-gray-300
+          ${error ? "border-red-500" : "border-gray-300 focus:border-green-100"}
+        }`}
         {...rest}
       />
+      {error && <span className="text-red-500 text-xs mt-1">{error}</span>}
     </fieldset>
   );
 }
