7e8654b948bdf7d7cff0b52539bbd54b34ffb9db
keewonf <lukasfmm@gmail.com>
2026-05-07 11:55:33 -0300

    feat(header): add logout button functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
 import logoSvg from "../assets/logo.svg";
 import logoutSvg from "../assets/logout.svg";
 
+import { useAuth } from "../hooks/useAuth";
+
 export function Header() {
+  const auth = useAuth();
+
   return (
     <header className="w-full flex justify-between">
       <img src={logoSvg} alt="logo" className="my-8" />
@@ -13,6 +17,7 @@ export function Header() {
           className="my-8 cursor-pointer hover:opacity-75 transition ease-linear"
           src={logoutSvg}
           alt="logout"
+          onClick={() => auth.remove()}
         />
       </div>
     </header>
