51c32282156e6c14aae6baf6c95feac950dce49c
keewonf <lukasfmm@gmail.com>
2026-05-07 11:10:53 -0300

    feat(auth): add AuthContext and session state

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import { createContext, type ReactNode } from "react";
 
-export const AuthContext = createContext({});
+type AuthContext = {
+  session: null | UserAPIResponse;
+};
+
+export const AuthContext = createContext({} as AuthContext);
 
 export function AuthProvider({ children }: { children: ReactNode }) {
+  const [session, setSession] = useState<null | UserAPIResponse>(null);
   return (
-    <AuthContext.Provider value={{ name: "Rodrigo" }}>
-      {children}
-    </AuthContext.Provider>
+    <AuthContext.Provider value={{ session }}>{children}</AuthContext.Provider>
   );
 }
