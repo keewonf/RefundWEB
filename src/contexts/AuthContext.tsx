93bdc05f0673ee3a22377c2a310ce4171bf92b7a
keewonf <lukasfmm@gmail.com>
2026-05-07 11:44:21 -0300

    feat(auth): add session persistence and loading state

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,21 +1,50 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { createContext, type ReactNode } from "react";
 
 type AuthContext = {
+  isLoading: boolean;
   session: null | UserAPIResponse;
   save: (data: UserAPIResponse) => void;
 };
+
+const LOCAL_STORAGE_KEY = "@refund";
 
 export const AuthContext = createContext({} as AuthContext);
 
 export function AuthProvider({ children }: { children: ReactNode }) {
   const [session, setSession] = useState<null | UserAPIResponse>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   function save(data: UserAPIResponse) {
+    localStorage.setItem(
+      `${LOCAL_STORAGE_KEY}:user`,
+      JSON.stringify(data.user),
+    );
     setSession(data);
+    localStorage.setItem(`${LOCAL_STORAGE_KEY}:token`, data.token);
   }
 
+  function loadUser() {
+    const user = localStorage.getItem(`${LOCAL_STORAGE_KEY}:user`);
+    const token = localStorage.getItem(`${LOCAL_STORAGE_KEY}:token`);
+
+    if (token && user) {
+      setSession({
+        token,
+        user: JSON.parse(user),
+      });
+    }
+
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    loadUser();
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ session, save }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ session, save, isLoading }}>
+      {children}
+    </AuthContext.Provider>
   );
 }
